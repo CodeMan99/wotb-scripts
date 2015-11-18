b03cad18d3fe00495131085477a68126c206262a
Cody Taylor <cody.taylor@maternityneighborhood.com>
2015-11-18 12:01:14 -0500

    What tanks have you kill in your played tanks?

---
+++ tank-frags.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+var async = require('async')
+  , wotb = require('wotblitz')
+
+async.auto({
+  sess: wotb.session.load,
+  all: (callback, d) => wotb.tankopedia.vehicles([], [], ['name', 'nation', 'tier'], callback),
+  login: ['sess', (callback, d) => d.sess.isLoggedIn() ? callback(null) : wotb.auth.login(8000, d.sess, callback)],
+  stats: ['sess', 'login', (callback, d) =>
+    wotb.tankStats.stats(null, [], null, ['frags', 'tank_id'], d.sess, callback)
+  ]
+}, (err, d) => {
+  if (err) throw err
+
+  var result = d.stats[d.sess.account_id]
+    .map(t => ({
+      name: d.all[t.tank_id].name,
+      nation: d.all[t.tank_id].nation,
+      tier: d.all[t.tank_id].tier,
+      frags: Object.keys(t.frags)
+        .sort((a, b) => t.frags[b] - t.frags[a])
+        .map(id => ({
+          [d.all[id].name]: t.frags[id]
+        }))
+    }))
+    .sort((a, b) => a.tier - b.tier)
+
+  if (process.stdout.isTTY) {
+    console.dir(result, {colors: true, depth: 3})
+  } else {
+    console.log(JSON.stringify(result, null, 2))
+  }
+})
