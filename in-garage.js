414fc7347012966777953b82d83bdab33c55fc88
Cody Taylor <cody.taylor@maternityneighborhood.com>
2015-12-11 14:48:12 -0500

    Respect hard limit imposed by vehicles endpoint.

--- in-garage.js
+++ in-garage.js
@@ -9,8 +9,24 @@ async.auto({
   stats: ['login', (callback, d) => wotb.tankStats.stats(null, [], 1, ['tank_id'], d.sess, callback)],
   garage: ['stats', (callback, d) => {
     var tankIds = d.stats[d.sess.account_id].map(s => s.tank_id)
+      , chunked = []
+      , limit = 100
+      , vehiclesPartial = (tankIdsChunk, cb) =>
+          wotb.tankopedia.vehicles(tankIdsChunk, [], ['is_premium', 'name', 'nation', 'tier', 'type'], cb)
 
-    wotb.tankopedia.vehicles(tankIds, [], ['is_premium', 'name', 'nation', 'tier', 'type'], callback)
+    if (tankIds.length <= limit) return vehiclesPartial(tankIds, callback)
+
+    for (var i = 0; i < tankIds.length; i += limit) {
+      chunked.push(tankIds.slice(i, i + limit))
+    }
+
+    async.map(chunked, vehiclesPartial, (err, chunkedData) => {
+      if (err) return callback(err)
+      callback(null, chunkedData.reduce((m, c) => {
+        Object.keys(c).forEach(k => m[k] = c[k])
+        return m
+      }, {}))
+    })
   }]
 }, (err, d) => {
   if (err) throw err
