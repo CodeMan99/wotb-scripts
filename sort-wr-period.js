a4ef592faf081cb4954fdb3865406f739c569577
Cody Taylor <codemister99@yahoo.com>
2017-08-25 01:49:56 -0400

    Sort low win rate tanks last with more battles.
    These tanks require more work to improve, so they should be lower
    in the least as the intention of the sort is help decide which
    tanks are most worth playing.
    
    High battle count is good for high win rate tanks since you can
    accomplish two goals at once (improving the tank, and the overall
    period).
    
    Also, convert to using a logger instance.

--- sort-wr-period.js
+++ sort-wr-period.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var concat = require('concat-stream');
+var logger = require('./lib/logger.js')();
 
 process.stdin.pipe(concat(buf => {
 	var period;
@@ -8,24 +9,22 @@ process.stdin.pipe(concat(buf => {
 	try {
 		period = JSON.parse(buf.toString());
 	} catch (e) {
-		return console.error(e.stack);
+		return logger.error(e);
 	}
 
 	var ordered = period.tanks.sort((a, b) => {
 		var A = a.wins / a.battles;
 		var B = b.wins / b.battles;
+		var direction = 1;
 
 		if (A < B) return 1;
 		if (A > B) return -1;
-		if (a.battles < b.battles) return 1;
-		if (a.battles > b.battles) return -1;
+		if (Math.round(A * 1000) < 501) direction = -1; // invert sort for tanks with less than 50.05%
+		if (a.battles < b.battles) return 1 * direction;
+		if (a.battles > b.battles) return -1 * direction;
 
 		return 0;
 	});
 
-	if (process.stdout.isTTY) {
-		console.dir(ordered, {colors: true});
-	} else {
-		console.log(JSON.stringify(ordered, null, 2));
-	}
+	logger.write(ordered);
 }));
