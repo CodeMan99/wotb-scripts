5a6a970be6cf89ae55525673d3e820c6040d2a0d
Cody Taylor <codemister99@yahoo.com>
2019-01-05 15:37:08 -0500

    Quieter error when stdin has no data.

--- filter-wr.js
+++ filter-wr.js
@@ -3,13 +3,21 @@
 var concat = require('concat-stream');
 var logger = require('./lib/logger.js');
 
+process.exitCode = 0;
 process.stdin.pipe(concat(buf => {
 	var period;
 	var tanks;
 
+	if (buf.length < 2) {
+		process.exitCode = 1;
+		return logger.error('filter-wr: input not reconized');
+	}
+
 	try {
 		period = JSON.parse(buf.toString());
 	} catch (e) {
+		process.exitCode = 2;
+		e.code = 'EJSON';
 		return logger.error(e);
 	}
 
@@ -18,6 +26,7 @@ process.stdin.pipe(concat(buf => {
 	} else if (Object.keys(period).length > 0) {
 		tanks = period;
 	} else {
+		process.exitCode = 1;
 		return logger.error('filter-wr: input not reconized');
 	}
 
